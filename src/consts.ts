bf4c4561b0a73325831198a279573d593d481ccb
Luiz Felipe <lfelipecarvalhedo@gmail.com>
2022-02-24 20:40:23 -0300

    moved files from root to floder

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-export const HOSTDB = process.env.HOSTDB;
-export const PORTDB = process.env.PORTDB;
-export const DBNAME = process.env.DBNAME;
-
-export const URI = "mongodb://" + HOSTDB + ":" + PORTDB;
--- tst/send-jwt.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const BASEURL = "http://localhost:3001";
-
-axios
-  .get(BASEURL + "/balance", { headers: { Authorization: "Bearer teste" } })
-  .then(console.log);
