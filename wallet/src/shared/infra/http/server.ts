2b9f4cc6d484218911d980cd91f0a5512eb4f8a0
Jonas Castro <jonascastro128@gmail.com>
2022-03-05 15:08:27 -0300

    feat(wallet): :sparkles: add cors

--- wallet/src/shared/infra/http/server.ts
+++ wallet/src/shared/infra/http/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import '@shared/infra/mongoose';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.listen(3001,()=>{
   console.log("Server started. Port 3001")
