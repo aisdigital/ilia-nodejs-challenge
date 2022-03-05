e63adc2a503ed376ceafa58c6306982cbd7c8c6f
Jonas Castro <jonascastro128@gmail.com>
2022-03-05 15:25:09 -0300

    feat(wallet): :sparkles:  create class AppError

--- wallet/src/shared/infra/http/server.ts
+++ wallet/src/shared/infra/http/server.ts
@@ -1,11 +1,28 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 
 import '@shared/infra/mongoose';
+import AppError from '@shared/erros/AppErros';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3001,()=>{
   console.log("Server started. Port 3001")
