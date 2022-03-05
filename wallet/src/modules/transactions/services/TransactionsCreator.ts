dd20e5a9b98c6357e21d0bfb3c347cde60a7079a
Jonas Castro <jonascastro128@gmail.com>
2022-03-05 17:25:53 -0300

    feat(wallet): :sparkles:  create TransactionsCreator

---
+++ wallet/src/modules/transactions/services/TransactionsCreator.ts
@@ -0,0 +1,17 @@
+import ITransactionsRepository from '@modules/transactions/repositories/ITransactionsRepository';
+import AppError from '@shared/errors/AppErros';
+import ICreateTransactionDTO from '../dtos/ICreateTransactionDTO';
+
+class TransactionsCreator {
+  constructor(private transactionsRepository: ITransactionsRepository) {}
+
+  async execute({ user_id, amount, type }: ICreateTransactionDTO) {
+    return this.transactionsRepository
+      .create({ user_id, amount, type })
+      .catch(() => {
+        throw new AppError('Erro ao registrar  transação.', 500);
+      });
+  }
+}
+
+export default TransactionsCreator;
