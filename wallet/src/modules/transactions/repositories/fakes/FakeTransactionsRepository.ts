f460aefd0272603ff4710af3aefeeb8386b11378
Jonas Castro <jonascastro128@gmail.com>
2022-03-05 16:22:56 -0300

    feat(wallet): :sparkles:  create FakeTransactionsRepository

---
+++ wallet/src/modules/transactions/repositories/fakes/FakeTransactionsRepository.ts
@@ -0,0 +1,29 @@
+
+import { Types } from 'mongoose';
+import ITransactionsRepository from '@modules/transactions/repositories/ITransactionsRepository';
+import ICreateTransactionDTO from '@modules/transactions/dtos/ICreateTransactionDTO';
+import TransactionEntity,  { ITransaction } from '@modules/transactions/infra/mongoose/entities/TransactionEntity';
+
+class FakeTransactionsRepository implements ITransactionsRepository {
+  private transactions: ITransaction[] = [];
+
+  public async find(): Promise<ITransaction[]> {
+    return this.transactions;
+  }
+
+  public async create(data: ICreateTransactionDTO): Promise<ITransaction> {
+        const transactionEntity = new TransactionEntity();
+
+    const transaction = Object.assign(transactionEntity, { 
+      id: new Types.ObjectId(),
+     ...data
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+}
+
+export default FakeTransactionsRepository;
