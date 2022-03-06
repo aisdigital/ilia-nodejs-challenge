080e926c8fea0a016a1f1f9c08f4cfa21518a97d
Jonas Castro <jonascastro128@gmail.com>
2022-03-06 18:45:56 -0300

    docs(wallet): :memo:  add wallet documentation

--- wallet/src/shared/infra/http/server.ts
+++ wallet/src/shared/infra/http/server.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
+import swaggerUi from 'swagger-ui-express';
 import app from './app';
+import swaggerDoc from '../../../swagger.json';
 
 const port = process.env.PORT || 3001;
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Server started. Port ${port}`);
