162695302f10937541bcb5c2179f5df53a10cc0c
Pedro Bonifacio <pedroboni.dev@gmail.com>
2022-04-03 19:18:17 -0300

    transaction routes

--- wallet/src/modules/transaction/routes.ts
+++ wallet/src/modules/transaction/routes.ts
@@ -1,5 +1,27 @@
+import { validationMiddleware } from '../../utils/error';
 import { Router } from 'express';
+import { createTransaction, listTransactions, getBalace } from './controller';
+import { validateCreateTransaction, validateListTransactions } from './validation';
+import passport from 'passport';
 
 const router = Router();
 
+router
+  .route('/transactions')
+  .post(
+    passport.authenticate('jwt', { session: false }),
+    validateCreateTransaction(),
+    validationMiddleware,
+    createTransaction,
+  )
+  .get(
+    passport.authenticate('jwt', { session: false }),
+    validateListTransactions(),
+    validationMiddleware,
+    listTransactions,
+  );
+
+router
+  .route('/balance')
+  .get(passport.authenticate('jwt', { session: false }), validationMiddleware, getBalace);
 export default router;
