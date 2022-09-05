1ce48f5c1c98e4119fc81180deef4e06e4ca1ffd
leoff00 <leof0180@gmail.com>
2022-09-05 11:08:20 -0300

    fix: updating deleted files

--- src/service/user-service/DeleteUserService.ts
+++
@@ -1,9 +0,0 @@
-import { IUserRepository } from "@repositories/user-repository/IUserRepository";
-
-export class DeleteUserService {
-  constructor(private readonly userRepository: IUserRepository) {}
-
-  async execute(id: string) {
-    return await this.userRepository.deleteUser(id);
-  }
-}
