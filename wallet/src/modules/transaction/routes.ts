55004454c5c4e3ed3a4d214dd80b45db2726e285
joaogoncalvesl5 <joaovictorlgoncalves5@gmail.com>
2022-09-13 16:12:21 -0300

    feature: create transaction

--- wallet/src/modules/transaction/routes.ts
+++ wallet/src/modules/transaction/routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express'
 import { validationMiddleware } from '../../middlewares/validationMiddleware'
 import { transactionController } from './controllers'
-import { validateCreateTransaction } from './validations'
+import { validateCreateTransaction, validateListTransactions } from './validations'
 import passport from 'passport'
 
 const router = Router()
 
 router.route('/')
+  .get(
+    passport.authenticate('jwt', { session: false }),
+    validateListTransactions(),
+    validationMiddleware,
+    transactionController.list
+  )
   .post(
     passport.authenticate('jwt', { session: false }), 
     validateCreateTransaction(), 
--- wallet/src/modules/transaction/validations.ts
+++ wallet/src/modules/transaction/validations.ts
@@ -1,6 +1,27 @@
-import { check, ValidationChain } from 'express-validator'
+import { check, query, ValidationChain } from 'express-validator'
 import { UserModel } from '../user/models/user'
 import { TransactionType } from './types'
+
+export const validateListTransactions = (): ValidationChain[] => [
+  query('receiving_user_id')
+    .optional()
+    .custom(async _id => {
+      const exists = await UserModel.exists({ _id })
+
+      if(!exists) return Promise.reject()
+      return Promise.resolve()
+    })
+    .withMessage('Receiving user does not exist'),
+  query('paying_user_id')
+    .optional()
+    .custom(async _id => {
+      const exists = await UserModel.exists({ _id })
+
+      if(!exists) return Promise.reject()
+      return Promise.resolve()
+    })
+    .withMessage('Paying user does not exist'),
+]
 
 export const validateCreateTransaction = (): ValidationChain[] => [
   check('receiving_user_id')
