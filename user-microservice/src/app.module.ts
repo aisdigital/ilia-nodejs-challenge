c9c5d544da35b0ed8956d31e0832b03c2237059f
Artur Paranayba <artur.paranayba@gmail.com>
2022-11-17 02:33:06 -0300

    chore(adequations): structural adequations

--- user-microservice/src/app.module.ts
+++ user-microservice/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from './shared/prisma/prisma.service';
-import { PrismaModule } from './shared/prisma/prisma.module';
-import { KafkaModule } from './shared/kafka/kafka.module';
+import { PrismaService } from '@shared/prisma/prisma.service';
+import { PrismaModule } from '@shared/prisma/prisma.module';
+import { KafkaModule } from '@shared/kafka/kafka.module';
 
 @Module({
   imports: [PrismaModule, KafkaModule],
