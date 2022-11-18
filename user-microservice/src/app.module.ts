8fddaf94033cc6a2799c0135305bef20216c29ee
Artur Paranayba <artur.paranayba@gmail.com>
2022-11-17 21:11:46 -0300

    chore(transactions): create module, controller and service

--- user-microservice/src/app.module.ts
+++ user-microservice/src/app.module.ts
@@ -4,13 +4,21 @@ import { EnvModule } from '@config/env/env.module';
 import { PrismaService } from '@shared/prisma/prisma.service';
 import { PrismaModule } from '@shared/prisma/prisma.module';
 import { KafkaModule } from '@shared/kafka/kafka.module';
-import { UserModule } from './app/user/user.module';
+import { UserModule } from '@app/user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guards/jwtAuth.guard';
+import { TransactionModule } from '@app/transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule, KafkaModule, EnvModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    KafkaModule,
+    EnvModule,
+    UserModule,
+    AuthModule,
+    TransactionModule,
+  ],
   controllers: [],
   providers: [
     PrismaService,
