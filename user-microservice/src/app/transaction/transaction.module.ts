e70bac903fd5393c2600624fab1341f848cd7292
Artur Paranayba <artur.paranayba@gmail.com>
2022-11-18 00:22:05 -0300

    feat(transaction): HttpModule added

--- user-microservice/src/app/transaction/transaction.module.ts
+++ user-microservice/src/app/transaction/transaction.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [TransactionController],
   providers: [TransactionService],
 })
