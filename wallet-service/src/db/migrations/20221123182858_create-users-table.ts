b69bc29213c5b8369b664bc1e147e500303ff392
Pedro Fontoura <pedrofontoura93@gmail.com>
2022-11-23 22:53:21 -0300

    fixed wrong column types on migrations

--- wallet-service/src/db/migrations/20221123182858_create-users-table.ts
+++ wallet-service/src/db/migrations/20221123182858_create-users-table.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
-        table.increments('id');
+        table.string('id').primary();
         table.string('first_name').notNullable();
         table.string('last_name').notNullable();
         table.string('password').notNullable();
