97cadc31d0b52df4ded9173169aa0418d7866f67
jhordy <jhooordy@gmail.com>
2023-12-12 18:09:34 -0300

    chore(user): update auth method, env.example

--- app/user/src/routes.ts
+++ app/user/src/routes.ts
@@ -3,7 +3,7 @@ import { login } from './services/auth';
 
 export const router = Router();
 
-router.use('auth', async (req, res) => {
+router.post('auth', async (req, res) => {
   const { email, password } = req.body;
   const user = await login({ email, password });
   res.status(200).json(user);
