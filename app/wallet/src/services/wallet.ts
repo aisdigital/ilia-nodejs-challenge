3a294080b78ca72d3f94a534bceb84a977b755a5
jhordy <jhooordy@gmail.com>
2023-12-13 14:04:37 -0300

    chore(wallet): select correct fields to return a new transaction

--- app/wallet/src/services/wallet.ts
+++ app/wallet/src/services/wallet.ts
@@ -4,6 +4,12 @@ import { walletDB } from '../utils/db';
 export const createTransaction = async (transaction: TransactionSchemaType) => {
   const createTransaction = await walletDB.transaction.create({
     data: transaction,
+    select: {
+      amount: true,
+      id: true,
+      user_id: true,
+      type: true,
+    },
   });
 
   return createTransaction;
