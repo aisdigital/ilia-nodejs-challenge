db671b73713176631e0a2b5cada9f716ab8bc66d
Fabio Bittencourt (Eneas) <fabio@ebittencourt.dev>
2023-12-21 15:42:33 -0300

    fix:remove unescessarie methods and routes

--- ms-transactions/src/transaction/transaction.service.ts
+++ ms-transactions/src/transaction/transaction.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { TransactionRepository } from './transaction.repository';
 
 @Injectable()
@@ -15,17 +14,5 @@ export class TransactionService {
 
   async findAll() {
     return await this.transactioRepository.getAllTransactions();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
-
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
   }
 }
