ef066e5ad10e4e03063db985c2b39f882a55fbfd
Fabio Bittencourt (Eneas) <fabio@ebittencourt.dev>
2023-12-22 23:13:37 -0300

    fix: removing comment

--- ms-users/src/main.ts
+++ ms-users/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //FIXME Alterar para usar variaveis por ambiente
   await app.listen(3002);
 }
 bootstrap();
