1991e6e6e248e4a81b2dff1b702f84455d9aa49b
rafaelalmeidav <rafael.4avv@gmail.com>
2026-02-12 17:03:52 -0300

    refactor: remove unused AppController and AppService, update AppModule with configuration and modules

--- wallet-service/src/app.module.ts
+++ wallet-service/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './modules/auth/auth.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
+import databaseConfig from './infrastructure/config/database.config';
+import jwtConfig from './infrastructure/config/jwt.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, jwtConfig],
+      envFilePath: '../.env'
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri')!,
+      }),
+    }),
+    AuthModule,
+    TransactionModule,
+  ],
 })
 export class AppModule {}
